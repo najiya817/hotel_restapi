f13eb34d67ea8281ad0ea3354893cc1990cb6fe5
najiya817 <najiyanaji817@gmail.com>
2023-03-14 14:19:22 +0530

    14mrch

---
+++ indiahotel/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+menu_items=[
+    {'id':1,'dish':"cb",'price':120,'rating':4.0},
+    {'id':2,'dish':"bb",'price':130,'rating':3.0},
+    {'id':3,'dish':"fr",'price':140,'rating':5.0},
+]
